73b3c8e8841d80ddad0caaa8a307d85fce8c03a1
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2021-05-02 20:54:37 -0300

    Muted and Unmuted

--- bot.js
+++ bot.js
@@ -3,7 +3,8 @@
 // ================================================================================================= //
 
 var storage = {
-  previous: []
+  previous: [],
+  prevMuted: []
 };
 
 // ================================================================================================= //
@@ -13,8 +14,14 @@ var storage = {
 function bot() {
   showUserList();
   const users = getUsers();
-  const result = prosses(users);
-  storage.previous = users.map((user) => user.name);
+  const userName = users.map((user) => user.name);
+  const mutedUser = users
+    .filter((user) => user.muted && user.name)
+    .map((user) => user.name);
+  const result = prosses(userName, mutedUser);
+
+  storage.previous = userName;
+  storage.prevMuted = mutedUser;
 }
 
 function getUsers() {
@@ -26,20 +33,23 @@ function getUsers() {
   });
 }
 
-function prosses(users) {
-  const userName = users.map((user) => user.name);
-  const mutedUser = users
-    .filter((user) => user.muted && user.name)
-    .map((user) => user.name);
+function prosses(userName, mutedUser) {
   const arrive = userName.filter(
     (user) => storage.previous.indexOf(user) === -1
   );
   const leave = storage.previous.filter(
     (user) => userName.indexOf(user) === -1
   );
+  const muted = mutedUser.filter(
+    (user) => storage.prevMuted.indexOf(user) === -1
+  );
+  const unmuted = storage.prevMuted.filter(
+    (user) => mutedUser.indexOf(user) === -1
+  );
   console.log("Arrive:", arrive);
   console.log("Leave:", leave);
-  console.log("Muted:", mutedUser);
+  console.log("Muted:", muted);
+  console.log("Unmuted:", unmuted);
 }
 
 // ================================================================================================= //
