8e4feb9bb832b4c0b32eafb737b0396208d62dcc
M4rcusVinicius <marcus.vinicius.mvap@gmail.com>
2021-05-04 12:46:00 -0300

    Correcting duplicate users error

--- robot.js
+++ robot.js
@@ -1,73 +1,108 @@
-  // ================================================================================================= //
-  //                                              Storage                                              //
-  // ================================================================================================= //
+// ================================================================================================= //
+//                                              Storage                                              //
+// ================================================================================================= //
 
-  var db = {
-    history: [],
-    prev: new Object,
-  } 
+var db = {
+  history: [],
+  prev: new Object(),
+};
 
-  // ================================================================================================= //
-  //                                             Listening                                             //
-  // ================================================================================================= //
+// ================================================================================================= //
+//                                             Listening                                             //
+// ================================================================================================= //
 
-  function bot() {
-    const users = getUsers();
-    const [ result, display ] = process(users)
-    console.log('Users to display:', display)
-    console.log('Result:', result)
-    db.prev = result
+function bot() {
+  const users = getUsers();
+  const [result, change] = process(users);
+  // console.log("Users to display:", change);
+  // console.log("Result:", result);
+  display(change)
+  db.prev = result;
+}
+
+function getUsers() {
+  const userList = document.getElementsByClassName("KV1GEc");
+  const names = new Array
+  if (userList.length === 0) {
+    error("Can't find any user");
   }
-
-  function getUsers() {
-    const userList = document.getElementsByClassName("KV1GEc");
-    if(userList.length === 0) { error("Can't find any user") }
-    return Object.values(userList).map((user) => {
-      const name = user.querySelector(".ZjFb7c").innerText;
-      const muted = !!user.querySelector(".FTMc0c");
-      return { name: name, muted: muted, att: [] };
-    });
-  }
-
-  function process(users) {
-    const result = new Object
-    const display = new Array
-
-    users.map(user => {
-      const id = user.name.replace(/[^\w\s]/gi, '').replace(/\s/g, '-')
-      if (!(id in db.prev)) {
-        { !(user.muted) && user.att.push('unmuted') }
-        user.att.push('new')
-      }
-      else if(db.prev[id].muted !== user.muted) {
-        if(user.muted) { user.att.push('muted') }
-        else { user.att.push('unmuted') }
-      };
-      delete db.prev[id]
-      console.log(user.name, ':', ...user.att)
-      if(user.att.length > 0) { display.push(user) }
-      result[id] = user
-    })
-
-    for (const [key, user] of Object.entries(db.prev)) {
-      user.att.push('leave')
-      console.log(user.name, ':', ...user.att)
-      display.push(user)
-      result[key] = user
+  return Object.values(userList).map((user) => {
+    let name = user.querySelector(".ZjFb7c").innerText;
+    if(names.indexOf(name) !== -1) {
+      name = name + ' (copy)'
+      error('There are 2 users with the same name, the name has been changed to ' + name)
+      user.querySelector('.ZjFb7c').innerText = name
+      console.log(user.querySelector('.ZjFb7c'))
     }
+    const muted = !!user.querySelector(".FTMc0c");
+    names.push(name)
+    return { name: name, muted: muted, att: [] };
+  });
+}
 
-    return [ result, display ]
+function process(users) {
+  const result = new Object();
+  const change = new Array();
+
+  users.map((user) => {
+    const id = user.name.replace(/[^\w\s]/gi, "").replace(/\s/g, "-");
+    if (!(id in db.prev)) {
+      {
+        !user.muted && user.att.push("unmuted");
+      }
+      user.att.push("new");
+    } else if (db.prev[id].muted !== user.muted) {
+      if (user.muted) {
+        user.att.push("muted");
+      } else {
+        user.att.push("unmuted");
+      }
+    }
+    delete db.prev[id];
+    // console.log(user.name, ":", ...user.att);
+    if (user.att.length > 0) {
+      change.push(user);
+    }
+    result[id] = user;
+  });
+
+  for (const [key, user] of Object.entries(db.prev)) {
+    user.att.push("leave");
+    // console.log(user.name, ":", ...user.att);
+    change.push(user);
+    result[key] = user;
   }
 
-  // ================================================================================================= //
-  //                                               Error                                               //
-  // ================================================================================================= //
+  return [result, change];
+}
 
-  function error(msg, err) {
-    console.warn('Error message:', msg)
-    if(err) { console.warn(err) }
+// ================================================================================================= //
+//                                              Assets                                               //
+// ================================================================================================= //
+
+function error(msg, err) {
+  console.warn("Error message:", msg);
+  if (err) {
+    console.warn(err);
   }
+}
 
-  console.clear()
-  bot()
-  bot()
+// ================================================================================================= //
+//                                              Display                                              //
+// ================================================================================================= //
+
+function display(change) {
+  const attColors = {
+    unmuted: "unmuted",
+    muted: "muted",
+    leave: "leave",
+    new: "new",
+  };
+  change.forEach((user) => {
+    console.log(user.name, ':', user.att);
+  });
+}
+
+console.clear();
+bot();
+bot();
